9b31a58408d73e7acb4ce6f27b6c1088bc466b0b
Robert Dorna <rdorna8@gmail.com>
2023-05-30 21:08:50 +0200

    feat: access to current Eats during Eats adding

--- src/ui/components/groups/products.jsx
+++ src/ui/components/groups/products.jsx
@@ -8,11 +8,22 @@ import { sizes, colors, globalStyles, useDatabase } from "../../../utils";
 
 
 export default function Products({ onCancel }) {
-  const { productsFilter, setProductsFilter, products } = useDatabase()
+  const { productsFilter, setProductsFilter, products, eats } = useDatabase()
   const [ newProduct, setNewProduct ] = React.useState(false)
+  const [ seeAlreadyAdded, setSeeAldeadyAdded ] = React.useState(false)
 
   return (
     <Group style={globalStyles.flex}>
+      <View style={styles.buttons}>
+        <Pressable style={[{ flexDirection: 'row' }]} onPress={() => setSeeAldeadyAdded(!seeAlreadyAdded)}>
+          <Icon name="eye" material focused={!!seeAlreadyAdded} />
+          <Text style={{ color: (seeAlreadyAdded === true) ? 'black' : 'grey', marginLeft: sizes.gap.small }}>see already added</Text>
+        </Pressable>
+        <Pressable style={[{ flexDirection: 'row', marginLeft: sizes.gap.large }]} onPress={onCancel}>
+          <Icon name="check" material focused />
+          <Text style={{ color: "black", marginLeft: sizes.gap.small }}>finish</Text>
+        </Pressable>
+      </View>
       <View style={{ flexDirection: 'row' }}>
         <TextInput
           style={styles.input}
@@ -27,15 +38,20 @@ export default function Products({ onCancel }) {
         </Pressable>
       </View>
       <ScrollView>
-        {products.map(p => {
-          return (<Product key={p.id} obj={p} />)
-        })}
-        {!!newProduct ?
+        {seeAlreadyAdded ? 
+          eats.map(p => {
+            return (<Product key={p.id} obj={p} />)
+          }) :
+          products.map(p => {
+            return (<Product key={p.id} obj={p} />)
+          })
+        }
+        {!seeAlreadyAdded && (!!newProduct ?
           <Product onDiscard={() => setNewProduct(false)}/> :
           <Button style={globalStyles.productButton} onPress={() => setNewProduct(true)}>
             <Icon name="plus" color="silver" focused material />
           </Button>
-        }
+        )}
         <View style={{height: 10}}/>
       </ScrollView>
     </Group>
@@ -50,5 +66,11 @@ const styles = StyleSheet.create({
     borderRadius: sizes.radius.mid,
     borderWidth: sizes.gap.line,
     borderColor: colors.palette.silver,
-  }
+  },
+  buttons: {
+    paddingTop: sizes.gap.large,
+    paddingBottom: sizes.gap.mid,
+    flexDirection: "row",
+    alignSelf: "center",
+  },
 })
