2aec7b404a3853c21effeb34e42be92bc1f8ba59
Robert Dorna <rdorna8@gmail.com>
2023-06-19 15:21:52 +0200

    fix: remove edit and add-eat buttons from new product wizard, refactor Product component

--- src/ui/components/product.jsx
+++ src/ui/components/product.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import { Pressable, View, Text, StyleSheet } from "react-native";
 import Icon from "./icon";
-import { sizes, colors, globalStyles, useDatabase } from "../../utils";
+import { sizes, colors, globalStyles, useDatabase, transform } from "../../utils";
 import { Editable } from "./setting";
 
 
-const transform = (obj, f) => Object.keys(obj).reduce((a, c) => ({ ...a, [c]: f(c, obj[c]) }), {})
-const stringify = obj => transform(obj, (_, v) => String(v))
+const toStringValues = obj => transform(obj, (_, v) => String(v))
 
 const emptyProduct = {
   name: "?",
@@ -19,38 +18,48 @@ const emptyProduct = {
   salt: 0
 }
 
+const ProductTypes = Object.freeze({
+  EatEditor: 1,
+  EatReader: 2,
+  EatAdder: 3,
+  ProductEditor: 4, 
+  ProductReader: 5,
+  ProductAdder: 6,
+})
 
 export default function Product({ obj = emptyProduct, onDiscard }) {
-  const creatorMode = obj === emptyProduct
-   
-  const { dayFilter, addEat, delEat, setEat, addProduct, setProduct, delProduct } = useDatabase()
+  const db = useDatabase();
+  const [edits, setEdits] = React.useState(obj === emptyProduct ? toStringValues(emptyProduct) : null)
   const [expanded, setExpanded] = React.useState(false)
-  const [edits, setEdits] = React.useState(!!creatorMode ? stringify(emptyProduct) : null)
   const [see100g, setSee100g] = React.useState(false)
 
-  const adjustByAmount = 'amount' in obj;
+  React.useEffect(() => { if (!edits) setSee100g(false) }, [edits])
+
+  const type =
+    (obj === emptyProduct) ? ProductTypes.ProductAdder :
+      ('amount' in obj) ?
+        (edits === null ? ProductTypes.EatReader : ProductTypes.EatEditor) :
+        (edits === null ?
+          ProductTypes.ProductReader :
+          ('amount' in edits ? ProductTypes.EatAdder : ProductTypes.ProductEditor)
+        );
+
   const fields = ['fat', 'saturated', 'carbs', 'sugar', 'protein', 'salt']
-
-  React.useEffect(() => {
-    if (!edits) {
-      setSee100g(false)
-    }
-  }, [edits])
-
   const expand = expanded || !!edits
 
   const onEditIconPress = ({ addAmount = false }) => {
-    if (!!edits) setExpanded(true)
-    const toStringify = addAmount ? { ...obj, amount: 100 } : obj
-    setEdits(!!edits ? null : stringify(toStringify))
+    if (edits !== null) setExpanded(true)
+    setEdits(edits !== null ? null : toStringValues(
+      addAmount ? { ...obj, amount: 100 } : obj
+    ))
   }
 
   const onDelete = () => {
-    if (!!adjustByAmount) {
-      delEat(obj.id)
+    if (type === ProductTypes.EatEditor) {
+      db.delEat(obj.id)
     }
-    else {
-      delProduct(obj.id)
+    else if (type === ProductTypes.ProductEditor) {
+      db.delProduct(obj.id)
     }
   }
 
@@ -67,18 +76,18 @@ export default function Product({ obj = emptyProduct, onDiscard }) {
           return v;
       }
     })
-    if (!!adjustByAmount) {
-      setEat(newObj)
+    if (type === ProductTypes.EatEditor) {
+      db.setEat(newObj)
     }
-    else {
-      setProduct(newObj)
+    else if (type === ProductTypes.ProductEditor) {
+      db.setProduct(newObj)
     }
     setExpanded(true)
     setEdits(null)
   }
 
   const onCreate = () => {
-    addProduct({
+    db.addProduct({
       name: String(edits.name),
       kcal: Number(edits.kcal),
       protein: Number(edits.protein),
@@ -91,131 +100,141 @@ export default function Product({ obj = emptyProduct, onDiscard }) {
     onDiscard()
   }
 
-  const onPress = () => {
-    if (!edits) {
-      setExpanded(!expanded)
-    }
-  }
+  const onPress = () => { if (!edits) setExpanded(!expanded) }
 
   const onCreateEat = () => {
-    addEat({
-      day: dayFilter,
+    db.addEat({
+      day: db.dayFilter,
       amount: edits.amount,
       productId: obj.id,
     })
   }
 
-  const onToggle100gView = () => {
-    setSee100g(!see100g)
+  const onToggle100gView = () => { setSee100g(!see100g) }
+
+  const options = {
+    [ProductTypes.EatEditor]: [
+      { name: ' save', icon: 'check-circle', cb: onSave },
+      { name: ' close', icon: 'close-circle', cb: onCancel },
+      { name: ' delete', icon: 'trash-can', cb: onDelete },
+    ],
+    [ProductTypes.EatReader]: null,
+    [ProductTypes.EatAdder]: [
+      { name: ' create', icon: 'check-circle', cb: onCreateEat },
+      { name: ' discard', icon: 'close-circle', cb: onCancel },
+      { name: ' see 100 g', icon: 'eye', cb: onToggle100gView, switchedOn: see100g },
+    ],
+    [ProductTypes.ProductEditor]: [
+      { name: ' save', icon: 'check-circle', cb: onSave },
+      { name: ' close', icon: 'close-circle', cb: onCancel },
+      { name: ' delete', icon: 'trash-can', cb: onDelete },
+    ],
+    [ProductTypes.ProductReader]: null,
+    [ProductTypes.ProductAdder]: [
+      { name: ' save', icon: 'check-circle', cb: onCreate },
+      { name: ' discard', icon: 'close-circle', cb: onDiscard },
+    ]
+  }[type]
+
+  const typesWith = {
+    editableProductName: [ProductTypes.ProductEditor, ProductTypes.ProductAdder],
+    ateAmountNextToProductName: [ProductTypes.EatEditor, ProductTypes.EatReader],
+
+    editableCalories: [ProductTypes.ProductEditor, ProductTypes.ProductAdder],
+    calculatedCalories: [ProductTypes.EatEditor, ProductTypes.EatReader, ProductTypes.EatAdder],
+
+    alwaysVisibleEditButton: [ProductTypes.EatEditor, ProductTypes.EatReader],
+    addEatButton: [ProductTypes.EatAdder, ProductTypes.ProductEditor, ProductTypes.ProductReader],
+
+    editableFields: [ProductTypes.ProductEditor, ProductTypes.ProductAdder],
+    calculatedFields: [ProductTypes.EatEditor, ProductTypes.EatReader, ProductTypes.EatAdder],
+
+    editableAmount: [ProductTypes.EatEditor, ProductTypes.EatAdder]
   }
 
-  const eatEditOptions = [
-    { name: ' save', icon: 'check-circle', cb: onSave },
-    { name: ' close', icon: 'close-circle', cb: onCancel },
-    { name: ' delete', icon: 'trash-can', cb: onDelete },
-  ]
-
-  const eatCreateOptions = [
-    { name: ' create', icon: 'check-circle', cb: onCreateEat },
-    { name: ' discard', icon: 'close-circle', cb: onCancel },
-    { name: ' see 100 g', icon: 'eye', cb: onToggle100gView, switchedOn: see100g },
-  ]
-
-  const productCreateOptions = [
-    { name: ' save', icon: 'check-circle', cb: onCreate },
-    { name: ' discard', icon: 'close-circle', cb: onDiscard },
-  ]
-
-  const productEditOptions = [
-    { name: ' save', icon: 'check-circle', cb: onSave },
-    { name: ' close', icon: 'close-circle', cb: onCancel },
-    { name: ' delete', icon: 'trash-can', cb: onDelete },
-  ]
-
-  const options = !!adjustByAmount ?
-    eatEditOptions : (!!edits && 'amount' in edits) ?
-      eatCreateOptions : creatorMode ?
-      productCreateOptions : 
-      productEditOptions;
+  const isOneOf = (type, types) => (types.indexOf(type) !== -1)
 
   const data = !!edits ? edits : obj;
 
   return (
     <Pressable style={styles.product} onPress={onPress}>
       <View style={globalStyles.spaced}>
-        {!adjustByAmount ?
-          (!!edits && !('amount' in edits)) ?
+        {
+          (isOneOf(type, typesWith.editableProductName)) ?
             <Editable
               ktype="default"
               style={[styles.editName, globalStyles.elevated]}
               input={edits.name}
               setInput={(v) => setEdits({ ...edits, name: String(v) })}
             /> :
-            <Text>{obj.name}</Text> :
-          <Text>{obj.name} ({obj.amount} g)</Text>
+            (isOneOf(type, typesWith.ateAmountNextToProductName)) ?
+              <Text>{obj.name} ({obj.amount} g)</Text> :
+              <Text>{obj.name}</Text>
         }
+
         <View style={{ flexDirection: 'row' }}>
-          {!!edits && !('amount' in edits) ?
-            <Editable
-              style={[styles.editName, globalStyles.elevated]}
-              input={edits.kcal}
-              setInput={(v) => setEdits({ ...edits, kcal: String(v) })}
-              postfix=" kcal"
-            /> :
-            (!adjustByAmount && !edits) ?
-              <Text style={{ marginRight: 10 }}>{obj.kcal} kcal</Text> :
-              <Text style={{ marginRight: 10 }}>{Math.round(obj.kcal / 100 * (see100g ? 100 : data.amount))} kcal</Text>
+          {
+            (isOneOf(type, typesWith.editableCalories)) ?
+              <Editable
+                style={[styles.editName, globalStyles.elevated]}
+                input={edits.kcal}
+                setInput={(v) => setEdits({ ...edits, kcal: String(v) })}
+                postfix=" kcal"
+              /> :
+              (isOneOf(type, typesWith.calculatedCalories)) ?
+                <Text style={{ marginRight: 10 }}>{Math.round(obj.kcal / 100 * (see100g ? 100 : data.amount))} kcal</Text> :
+                <Text style={{ marginRight: 10 }}>{obj.kcal} kcal</Text>
           }
-          {(!!expand && !adjustByAmount) &&
-            <Pressable onPress={onEditIconPress} style={{ marginRight: 10 }} disabled={!!edits}>
-              <Icon name="circle-edit" material color={!!edits ? 'grey' : 'black'}/>
-            </Pressable>
-          }
-          {!!adjustByAmount ?
-            <Pressable onPress={() => onEditIconPress({ addAmount: false })} disabled={!!edits}>
-              <Icon name="circle-edit" material color={!!edits ? 'grey' : 'black'} />
-            </Pressable> :
-            <Pressable onPress={() => onEditIconPress({ addAmount: true })} disabled={!!edits}>
-              <Icon name={"plus-circle"} material color={!!edits ? 'grey' : 'black'} />
-            </Pressable>
+          {
+            (type !== ProductTypes.ProductAdder) &&
+            (<>
+              {
+                (expand || isOneOf(type, typesWith.alwaysVisibleEditButton)) ?
+                  <Pressable onPress={onEditIconPress} style={{ marginRight: 10 }} disabled={!!edits}>
+                    <Icon name="circle-edit" material color={!!edits ? 'grey' : 'black'} />
+                  </Pressable> : <></>
+              }
+              {
+                (isOneOf(type, typesWith.addEatButton)) ?
+                  <Pressable onPress={() => onEditIconPress({ addAmount: true })} disabled={!!edits}>
+                    <Icon name={"plus-circle"} material color={!!edits ? 'grey' : 'black'} />
+                  </Pressable> : <></>
+              }
+            </>)
           }
         </View>
+
       </View>
       {expand &&
         <View style={styles.grid}>
-          {fields.map(field => {
-            if (!edits || 'amount' in edits) {
-              return (
-                <View key={field} style={[styles.field, globalStyles.spaced]}>
-                  <Text>{field}</Text>
-                  {(!adjustByAmount && (!edits || !('amount' in edits))) ?
-                    <Text>{obj[field]} g</Text> :
-                    <Text>{Math.round(obj[field] / 100 * (see100g ? 100 : data.amount))} g</Text>
-                  }
-                </View>
-              )
-            }
-            return (
-              <Editable
-                key={field}
-                style={[styles.editField, globalStyles.spaced, globalStyles.elevated]}
-                input={edits[field]}
-                setInput={(v) => setEdits({ ...edits, [field]: String(v) })}
-                postfix=" g"
-              >{field}</Editable>
-            )
-          })}
+          {fields.map(field => isOneOf(type, typesWith.editableFields) ?
+            <Editable
+              key={field}
+              style={[styles.editField, globalStyles.spaced, globalStyles.elevated]}
+              input={edits[field]}
+              setInput={(v) => setEdits({ ...edits, [field]: String(v) })}
+              postfix=" g"
+            >{field}</Editable>
+            :
+            <View key={field} style={[styles.field, globalStyles.spaced]}>
+              <Text>{field}</Text>
+              {(isOneOf(type, typesWith.calculatedFields)) ?
+                <Text>{Math.round(obj[field] / 100 * (see100g ? 100 : data.amount))} g</Text> :
+                <Text>{obj[field]} g</Text>
+              }
+            </View>
+          )}
         </View>
       }
-      {(!!edits && 'amount' in edits) &&
+      {isOneOf(type, typesWith.editableAmount) &&
         <Editable
           style={[styles.editAmount, globalStyles.spaced, globalStyles.elevated]}
           input={edits.amount}
           setInput={(v) => setEdits({ ...edits, amount: String(v) })}
           postfix=" g"
-        >{!!adjustByAmount ? 'new' : 'ate'} amount: </Editable>
+        >{(type === ProductTypes.EatEditor) ? 'new' : 'ate'} amount: </Editable>
       }
-      {!!edits &&
+      {options &&
         <View style={{ flexDirection: 'row', justifyContent: "space-evenly", marginTop: 10 }}>
           {options.map(({ name, icon, cb, switchedOn }) => (
             <Pressable key={name} style={[{ flexDirection: 'row' }]} onPress={cb}>
